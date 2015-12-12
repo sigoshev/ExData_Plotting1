f18be7888cab337b6886735f48fdd2706a659376
Stanislav Igoshev <igoshevstas@gmail.com>
2015-12-12 23:47:27 +0300

    Combine filter() and mutate() together with the pipe operator

--- plot4.R
+++ plot4.R
@@ -5,10 +5,7 @@ household_power_consumption <-
              na.strings="?", stringsAsFactors=FALSE)
 household_power_consumption <- 
     household_power_consumption %>%
-    filter(Date == "1/2/2007" | Date == "2/2/2007")
-
-household_power_consumption <-
-    household_power_consumption %>%
+    filter(Date == "1/2/2007" | Date == "2/2/2007") %>%
     mutate(DateTime = as.POSIXct(paste(Date, Time), 
                                  format="%d/%m/%Y %H:%M:%S"))
 
